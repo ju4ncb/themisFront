55f99b9110f3b4e1bd73dd799027ff1b7bab39cc
ju4ncb <ju4ncb@gmail.com>
2025-05-21 00:06:38 -0500

    added responsive design to login and register

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
 import blueBg from "../assets/blue-background.jpg";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const HomeLayout = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  useEffect(() => {
+    console.log(currentUrl);
+  }, []);
   return (
     <div className="darker-bg fullscreen grid-center">
       <div className="auth-layout">
-        <section className="auth-content">
+        <section
+          className={
+            currentUrl === "/register"
+              ? "auth-content width-100"
+              : "auth-content"
+          }
+        >
           <Outlet />
         </section>
         <section className="auth-img">

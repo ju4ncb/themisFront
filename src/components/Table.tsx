f21fc0302ecfbba6f173f1646f0bc0d7b4318d7a
ju4ncb <ju4ncb@gmail.com>
2025-05-23 01:24:23 -0500

    added table on home and finished home

---
+++ src/components/Table.tsx
@@ -0,0 +1,81 @@
+import { type LucideProps } from "lucide-react";
+import React, { useState } from "react";
+
+interface Action {
+  Icon: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+  actionFunction: ((row: any) => void) | ((row: any) => Promise<void>);
+}
+
+interface Props {
+  data: any;
+  actions?: Action[];
+  ignoreId?: boolean;
+}
+
+const ROWS_PER_PAGE = 20;
+
+const Table: React.FC<Props> = ({ data, actions, ignoreId }) => {
+  const [page, setPage] = useState(1);
+
+  const totalRows = data.length;
+  const totalPages = Math.ceil(totalRows / ROWS_PER_PAGE);
+
+  const paginatedData = data.slice(
+    (page - 1) * ROWS_PER_PAGE,
+    page * ROWS_PER_PAGE
+  );
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            {Object.keys(data[0] || {}).map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+            {actions && <th>Actions</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {paginatedData.map((row: Record<string, any>, index: number) => (
+            <tr key={index + (page - 1) * ROWS_PER_PAGE}>
+              {Object.keys(row)
+                .filter(
+                  (key) => !(ignoreId && key.toLowerCase().startsWith("id"))
+                )
+                .map((key) => (
+                  <td key={key}>{row[key]}</td>
+                ))}
+              {actions && (
+                <td className="actions">
+                  {actions.map(({ Icon, actionFunction }, i) => (
+                    <Icon key={i} onClick={() => actionFunction(row)} />
+                  ))}
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="table-buttons">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => setPage(i + 1)}
+            disabled={page === i + 1}
+            style={{
+              margin: "0 4px",
+              fontWeight: page === i + 1 ? "bold" : "normal",
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Table;
--- src/pages/Dashboard/home/home.tsx
+++ src/pages/Dashboard/home/home.tsx
@@ -5,15 +5,23 @@ import CardButton from "../../../components/CardButton";
 import "./home.scss";
 import { useArchivoSalarial } from "../../../contexts/ArchivoSalarialContext";
 import type { ArchivoSalarial } from "../../../models/ArchivoSalarial";
+import Table from "../../../components/Table";
+import { Trash2 } from "lucide-react";
+import Swal from "sweetalert2";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 const Dashboard: React.FC = () => {
-  const { archivoSalarial } = useArchivoSalarial();
+  const { archivoSalarial, setArchivoSalarial } = useArchivoSalarial();
   const [isDetalles, setIsDetalles] = useState(true);
   const [navBarActive, setNavBarActive] = useState(false);
   const [archivosSalariales, setArchivosSalariales] = useState<
     ArchivoSalarial[]
   >([]);
+  const [numFilas, setNumFilas] = useState<string | number>("Cargando...");
+  const [numColumnas, setNumColumnas] = useState<string | number>(
+    "Cargando..."
+  );
   useEffect(() => {
     const loadDatasets = async () => {
       const response = await fetch(`${API_URL}/archivossalariales`);
@@ -27,6 +35,59 @@ const Dashboard: React.FC = () => {
     }
     loadDatasets();
   }, [archivoSalarial]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (archivoSalarial && Array.isArray(archivoSalarial.contenido)) {
+        const contenido = archivoSalarial.contenido;
+        setNumFilas(contenido.length);
+        setNumColumnas(
+          contenido.length > 0
+            ? Object.keys(JSON.parse(contenido[0].fila_registro)).length
+            : 0
+        );
+      } else {
+        setNumFilas("Cargando...");
+        setNumColumnas("Cargando...");
+      }
+    }, 50);
+    return () => clearTimeout(timeout);
+  }, [archivoSalarial]);
+  const deleteRow = async (id_registro: number) => {
+    try {
+      const response = await fetch(
+        `${API_URL}/registrossalariales/${id_registro}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        Swal.fire({
+          icon: "success",
+          title: "Eliminación exitosa",
+          text: `Fila eliminada con éxito.`,
+        }).then(() => {
+          if (!archivoSalarial) return;
+          setArchivoSalarial({
+            ...archivoSalarial,
+            contenido: archivoSalarial!.contenido!.filter(
+              (fila: { id_registro: number }) =>
+                fila.id_registro !== id_registro
+            ),
+          });
+        });
+      } else {
+        // Handle error
+        throw Error();
+      }
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Hubo un error inesperado eliminando la fila.",
+      });
+    }
+  };
   return (
     <div className={navBarActive ? "dashboard with-navbar" : "dashboard"}>
       {navBarActive && (
@@ -35,67 +96,89 @@ const Dashboard: React.FC = () => {
           setIsDetalles={setIsDetalles}
         />
       )}
-      <main className="dashboard__content">
-        <SubirCargarArchivo
-          archivosSalariales={archivosSalariales}
-          setNavBarActive={setNavBarActive}
-        />
-        <div className="dashboard__cards">
-          {archivoSalarial === null ? (
-            <>
-              <CardButton title="Último dataset usado" buttonText="Usar este">
-                <span className="card__name">&lt;Nombre-archivo&gt;</span>{" "}
-                <span className="card__attr">&lt;fecha&gt;</span>{" "}
-                <span className="card__attr">&lt;tamaño&gt;</span>
-              </CardButton>
+      {isDetalles ? (
+        <main className="dashboard__content">
+          <SubirCargarArchivo
+            archivosSalariales={archivosSalariales}
+            setNavBarActive={setNavBarActive}
+          />
+          <div className="dashboard__cards">
+            {archivoSalarial === null ? (
+              <>
+                <CardButton title="Último dataset usado" buttonText="Usar este">
+                  <span className="card__name">&lt;Nombre-archivo&gt;</span>{" "}
+                  <span className="card__attr">&lt;fecha&gt;</span>{" "}
+                  <span className="card__attr">&lt;tamaño&gt;</span>
+                </CardButton>
 
-              <CardButton title="Buscar datasets" buttonText="Entrar a kaggle">
-                <span className="card__name">Buscar datasets en Kaggle</span>
-              </CardButton>
-            </>
-          ) : (
-            <>
-              <CardButton title="Número de filas">
-                <span className="card_number">
-                  {archivoSalarial.contenido?.length}
-                </span>
-              </CardButton>
-              <CardButton title="Número de columnas">
-                <span className="card_number">
-                  {archivoSalarial.contenido
-                    ? Object.keys(archivoSalarial.contenido[0]).length
-                    : 0}
-                </span>
-              </CardButton>
-              <CardButton title="Tamaño">
-                <span className="card_number">
-                  {(archivoSalarial.tamano / 1024).toFixed(2)} kB
-                </span>
-              </CardButton>
-              <CardButton title="Fecha creado">
-                <span className="card_number">
-                  {(() => {
-                    const date = new Date(archivoSalarial.created_at);
-                    const year = date.getFullYear();
-                    const month = date.getMonth() + 1;
-                    const day = date.getDate();
-                    let hours = date.getHours();
-                    const minutes = String(date.getMinutes()).padStart(2, "0");
-                    const seconds = String(date.getSeconds()).padStart(2, "0");
-                    const ampm = hours >= 12 ? "p.m." : "a.m.";
-                    hours = hours % 12;
-                    hours = hours ? hours : 12; // the hour '0' should be '12'
-                    return `${year}-${month}-${day} ${String(hours).padStart(
-                      2,
-                      "0"
-                    )}:${minutes}:${seconds} ${ampm}`;
-                  })()}
-                </span>
-              </CardButton>
-            </>
-          )}
-        </div>
-      </main>
+                <CardButton
+                  title="Buscar datasets"
+                  buttonText="Entrar a kaggle"
+                >
+                  <span className="card__name">Buscar datasets en Kaggle</span>
+                </CardButton>
+              </>
+            ) : (
+              <>
+                <CardButton title="Número de filas">
+                  <span className="card_number">{numFilas}</span>
+                </CardButton>
+                <CardButton title="Número de columnas">
+                  <span className="card_number">{numColumnas}</span>
+                </CardButton>
+                <CardButton title="Tamaño">
+                  <span className="card_number">
+                    {(archivoSalarial.tamano / 1024).toFixed(2)} kB
+                  </span>
+                </CardButton>
+                <CardButton title="Fecha creado">
+                  <span className="card_number">
+                    {(() => {
+                      const date = new Date(archivoSalarial.created_at);
+                      const year = date.getFullYear();
+                      const month = date.getMonth() + 1;
+                      const day = date.getDate();
+                      let hours = date.getHours();
+                      const minutes = String(date.getMinutes()).padStart(
+                        2,
+                        "0"
+                      );
+                      const seconds = String(date.getSeconds()).padStart(
+                        2,
+                        "0"
+                      );
+                      const ampm = hours >= 12 ? "p.m." : "a.m.";
+                      hours = hours % 12;
+                      hours = hours ? hours : 12; // the hour '0' should be '12'
+                      return `${year}-${month}-${day} ${String(hours).padStart(
+                        2,
+                        "0"
+                      )}:${minutes}:${seconds} ${ampm}`;
+                    })()}
+                  </span>
+                </CardButton>
+              </>
+            )}
+          </div>
+        </main>
+      ) : (
+        <main className="dashboard__table">
+          <Table
+            data={archivoSalarial?.contenido?.map(
+              ({ fila_registro, id_registro }) => ({
+                ...JSON.parse(fila_registro),
+                id_registro, // include id_registro for actions
+              })
+            )}
+            actions={[
+              {
+                actionFunction: (row) => deleteRow(row.id_registro),
+                Icon: Trash2,
+              },
+            ]}
+          />
+        </main>
+      )}
     </div>
   );
 };
